2c5a2f36da58a50d6e2ec5cf4f743f1875439e27
ynero <ynero@softserveinc.com>
2021-08-30 15:21:51 +0300

    feature/SS-75: Updated kafka consumer with new functionality

--- src/main/java/com/ynero/ss/analytics_service/service/device/DeviceKafkaConsumer.java
+++ src/main/java/com/ynero/ss/analytics_service/service/device/DeviceKafkaConsumer.java
@@ -1,27 +1,34 @@
 package com.ynero.ss.analytics_service.service.device;
 
+import com.ynero.ss.analytics_service.domain.PortsDataRecord;
+import com.ynero.ss.analytics_service.persistence.PortsDataRecordRepository;
 import dtos.PortValueDTO;
 import json_converters.DTOToMessageJSONConverter;
 import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Service
 @Log4j2
 public class DeviceKafkaConsumer {
 
     private final DTOToMessageJSONConverter<PortValueDTO> converter;
+    private final ModelMapper modelMapper;
+    private final PortsDataRecordRepository repository;
 
-    public DeviceKafkaConsumer(DTOToMessageJSONConverter<PortValueDTO> converter) {
+    public DeviceKafkaConsumer(DTOToMessageJSONConverter<PortValueDTO> converter, ModelMapper modelMapper, PortsDataRecordRepository repository) {
         this.converter = converter;
+        this.modelMapper = modelMapper;
+        this.repository = repository;
     }
 
 
     @KafkaListener(topics = "device-data-for-save", groupId = "device-data-for-save_group_id")
     public void consume(String message) {
-        PortValueDTO portValue= (PortValueDTO) converter.deserialize(message, PortValueDTO.class);
-        log.info("new log: {}", portValue);
+        PortValueDTO portValue = (PortValueDTO) converter.deserialize(message, PortValueDTO.class);
+        var portsDataRecord = modelMapper.map(portValue, PortsDataRecord.class);
+        portsDataRecord = repository.save(portsDataRecord);
+        log.info("new log: {}", portsDataRecord);
     }
 }
