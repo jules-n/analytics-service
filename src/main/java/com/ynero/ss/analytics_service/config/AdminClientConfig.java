f1652d01c5255bddcec19afc040d0706eb49bcb0
ynero <ynero@softserveinc.com>
2021-10-05 17:24:54 +0300

    feature/SS-33: Added bean for kafka client

---
+++ src/main/java/com/ynero/ss/analytics_service/config/AdminClientConfig.java
@@ -0,0 +1,30 @@
+package com.ynero.ss.analytics_service.config;
+
+import lombok.Setter;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Configuration
+public class AdminClientConfig {
+
+    @Setter(onMethod_ = {@Value("${spring.kafka.producer.bootstrap-servers}")})
+    private String bootstrapServers;
+
+    @Setter(onMethod_ = {@Autowired})
+    private KafkaAdmin kafkaAdmin;
+
+    @Bean
+    public AdminClient getAdminClient(){
+        var properties = new HashMap<String, Object>();
+        properties.putAll(kafkaAdmin.getConfigurationProperties());
+        properties.put("bootstrap.servers", List.of(bootstrapServers));
+        return AdminClient.create(properties);
+    }
+}
